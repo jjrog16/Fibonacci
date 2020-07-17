641ffb69904e314c546a05b406ef9e15ca57ab77
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-07-16 22:43:41 -0400

    Implemented Adapter to take in the TextView

--- app/src/main/java/com/example/android/fibonacci/FibNumAdapter.kt
+++ app/src/main/java/com/example/android/fibonacci/FibNumAdapter.kt
@@ -1,2 +1,30 @@
 package com.example.android.fibonacci
 
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import timber.log.Timber
+import java.math.BigInteger
+
+class FibNumAdapter(private val fibonacciResult: Array<BigInteger>):
+        RecyclerView.Adapter<FibNumAdapter.FibNumViewHolder>(){
+
+    class FibNumViewHolder(val textView: TextView):RecyclerView.ViewHolder(textView)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): FibNumAdapter.FibNumViewHolder {
+        val textView = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item, parent, false) as TextView
+        return FibNumViewHolder(textView)
+    }
+
+    override fun getItemCount() = fibonacciResult.size
+
+    override fun onBindViewHolder(holder: FibNumAdapter.FibNumViewHolder, position: Int) {
+        Timber.i("fibonacci position: $position -> ${fibonacciResult[position]}")
+        holder.textView.text = fibonacciResult[position].toString()
+    }
+}
--- app/src/main/java/com/example/android/fibonacci/ui/FibonacciFragment.kt
+++ app/src/main/java/com/example/android/fibonacci/ui/FibonacciFragment.kt
@@ -7,28 +7,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.fibonacci.FibNumAdapter
 import com.example.android.fibonacci.R
+import kotlinx.android.synthetic.main.fibonacci_fragment.*
 import timber.log.Timber
+import java.math.BigInteger
 
 class FibonacciFragment : Fragment() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
     private lateinit var viewModel: FibonacciViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fibonacci_fragment, container, false)
+        val view = inflater.inflate(R.layout.fibonacci_fragment, container, false)
+
+        viewModel = ViewModelProvider(this).get(FibonacciViewModel::class.java)
+
+        recyclerView = view.findViewById(R.id.fib_recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = FibNumAdapter(viewModel.fibonacciResult)
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(FibonacciViewModel::class.java)
         // TODO: Use the ViewModel
         // Check to see results are showing as expected
-        for (i in 0 until 1000){
-            Timber.i("$i: ${viewModel.fibonacciResult[i]}")
-        }
 
         //TODO: Create Adapter that handles RecyclerView
 
